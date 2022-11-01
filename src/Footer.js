da4bb78cd6de037ab1b3a49a2cace06a4f84b0bc
J M Cabrera Elias <jmcabrerae@gmail.com>
2022-10-31 18:32:28 -0700

    updated read me

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 class Footer extends React.Component {
   render() {
     return (
+    
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>Code Fellows</Navbar.Brand>
       </Navbar>
