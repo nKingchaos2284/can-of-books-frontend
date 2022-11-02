d8345b1dd68fbfd5a780fd93cc92d2fbd879f225
J M Cabrera Elias <jmcabrerae@gmail.com>
2022-11-02 16:52:23 -0700

    updated and pretty much finished front end, waiting for back to catch up

--- src/components/BooksCarousel.js
+++ src/components/BooksCarousel.js
@@ -9,16 +9,17 @@ class BooksCarousel extends React.Component {
     for (let i = 0; i < this.props.books.length; i++) {
       items.push(
         <Carousel.Item key = {this.props.books[i]._id} className = {styles.BooksCarousel__container}>
-          <img
+            <img
             className= {`d-block w-50 ${styles.BooksCarousel}`}
-            src="https://www.freepik.com/free-psd/open-book-mockup_12953935.htm#query=books&position=30&from_view=search&track=sph"
+            src="https://s26162.pcdn.co/wp-content/uploads/sites/2/2022/05/Book.jpg"
             alt={this.props.books[i].title}
           />
-          <Carousel.Caption>
+           <Carousel.Caption>
             <h3>{this.props.books[i].title}</h3>
             <p>{this.props.books[i].description}</p>
-            <p>{this.props.books[i].available ? 'We have this on the shelf!' : 'Apologies, we\'ll have to order this.'}</p>
-            <Button id = {this.props.books[i]._id} variant='light' onClick={(e) => this.props.deleteHandler(e.target.id)}>Delete Book</Button>
+            <p>{this.props.books[i].available ? 'We have this in stock!' : 'Sorry, we\'ll have to order this.'}</p>
+            <Button id = {this.props.books[i]._id} variant='danger' onClick={(e) => this.props.deleteHandler(e.target.id)}>Delete Book</Button>
+            <Button id = {this.props.books[i]._id} variant='light' onClick={(e) => this.props.updateHandler(e.target.id)}>Edit Book</Button>
           </Carousel.Caption>
         </Carousel.Item>
       );
